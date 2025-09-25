72219624e48f8e80cae4b31201f5ed1bc8ea2e91
drive <driveasano@gmail.com>
2025-09-25 17:40:10 +0900

    Replace Geist font with Noto Sans JP in layout and update body class for improved styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
+const notoSansJP = Noto_Sans_JP({
+    variable: "--font-noto-sans-jp",
     subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+    weight: ["400", "500", "700"],
 });
 
 export const metadata: Metadata = {
@@ -26,9 +23,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="ja">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
+            <body className={cn(notoSansJP.variable, " antialiased")}>
                 {children}
                 <SpeedInsights />
                 <Analytics />
